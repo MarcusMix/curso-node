8199da1aef9155481d34598b69fae6b4f5b8b413
Marcus Sandi <tradedomarcus@gmail.com>
2023-03-03 14:27:16 -0300

    feat: example of event loop

---
+++ 2-fundamentos/9-event-loop/index.mjs
@@ -0,0 +1,22 @@
+function umaFuncaoExemplo() {
+    console.log("executou a funcao de exemplo")
+}
+
+
+function umaFuncaoExemplo2() {
+    console.log("executou a funcao de exemplo 2")
+}
+
+
+function umaFuncaoExemplo3() {
+    console.log("executou a funcao de exemplo 3")
+}
+
+umaFuncaoExemplo()
+umaFuncaoExemplo2()
+umaFuncaoExemplo3()
+
+/*
+garante que a execução do código seja sequencial.
+de cima para baixo, garantindo a execução de uma instrução ordenada
+*/
