efabc589b2122855074d58ef756cab67aefb7e9c
Marcus Sandi <tradedomarcus@gmail.com>
2023-03-04 16:05:17 -0300

    refactor: file of createAccount function

---
+++ 5-accounts/functions/createAccount.mjs
@@ -0,0 +1,8 @@
+import chalk from "chalk";
+
+const createAccount = () => {
+	console.log(chalk.bgGreen.black("Obrigado por usar o Accounts"));
+	console.log(chalk.green("Defina as configurações da sua conta:"));
+};
+
+export default createAccount
