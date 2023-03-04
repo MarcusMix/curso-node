cfee9833dcffa23b7d13cfa4ebcc0ffe96ff8a9a
Marcus Sandi <tradedomarcus@gmail.com>
2023-03-04 11:59:25 -0300

    feat: module os, info about pc and user

---
+++ 3-core-modules/15-module-os/index.mjs
@@ -0,0 +1,20 @@
+import os from 'os'
+
+//info sobre cpf
+console.log(os.cpus())
+
+//info sobre memoria ram
+console.log(os.freemem())
+
+//info sobre usuário
+console.log(os.homedir())
+console.log(os.userInfo())
+
+//info sobre sistema operacional
+console.log(os.type())
+
+//host
+console.log(os.hostname())
+
+//info sobre arquitetura
+console.log(os.arch())
