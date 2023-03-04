95b96dd14c9766eac6e75393916fb8b1431db4c1
Marcus Sandi <tradedomarcus@gmail.com>
2023-03-04 11:12:34 -0300

    feat: appendFile using user props

---
+++ 3-core-modules/7-appendFile/index.mjs
@@ -0,0 +1,37 @@
+import http from 'http'
+import fs from 'fs'
+import url from 'url'
+
+const porta = 3000
+
+const server = http.createServer((req, res) => {
+
+    const urlInfo = url.parse(req.url, true)
+    const name = urlInfo.query.name
+    const newNameLine = name + '\r\n'
+
+    if(!name) {
+        fs.readFile('index.html', function (error, data) {
+            try {
+                res.writeHead(200, {'Content-Type' : 'text/html'})
+                res.write(data)
+                return res.end()
+            } catch (error) {
+                console.log(error)
+            }
+        })
+    } else {
+        fs.appendFile('arquivo.txt', newNameLine, function(error, data) {
+            res.writeHead(302, {
+                Location: '/'
+            })
+            return res.end()
+        })
+    }
+
+    
+})
+
+server.listen(porta, () => {
+    console.log(`servidor rodando na porta ${porta}`)
+})
