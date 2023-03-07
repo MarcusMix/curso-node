e51091a537419dc05fbec6ee423185e53fb9f06c
Marcus Sandi <tradedomarcus@gmail.com>
2023-03-07 15:11:48 -0300

    feat: post book details

--- 8-mysql/setup/index.js
+++ 8-mysql/setup/index.js
@@ -4,21 +4,54 @@ const mysql = require("mysql");
 
 const app = express();
 
+//pegar os dados do body
+app.use(
+	express.urlencoded({
+		extended: true,
+	})
+);
+
+app.use(express.json());
+
+//statics
+app.use(express.static("public"));
+
+//handlebars setup
 app.engine("handlebars", exphbs.engine());
 app.set("view engine", "handlebars");
 
+//books page POST
+app.post("/books/insertbook", (req, res) => {
+	const title = req.body.title;
+	const pages = req.body.pages;
+
+	const query = `INSERT INTO books (title, pages) VALUES (?)`;
+	const values = [title, pages];
+
+	connection.query(query, [values], (error) => {
+		if (error) {
+			console.log(error);
+		}
+
+		res.redirect("/");
+	});
+});
+
+//page
 app.get("/", (req, res) => {
 	res.render("home");
 });
 
+//get connection with mysql
 const connection = mysql.createConnection({
 	host: "localhost",
 	user: "root",
-	password: "admin",
+	password: "",
 	database: "dbteste",
+	waitForConnection: true,
 });
 
-connection.connect(function (error) {
+connection.connect((error) => {
 	if (error) {
 		console.log(error);
 	}
