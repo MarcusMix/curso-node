12e36dc5300fd1931f7ad4d50f5961f8fd587f1a
Marcus Sandi <tradedomarcus@gmail.com>
2023-03-13 13:08:37 -0300

    feat:  route of remove, edit and done

--- 10-mvc/routes/taskRoutes.js
+++ 10-mvc/routes/taskRoutes.js
@@ -6,5 +6,9 @@ import TaskController from "../controllers/TaskController.js"
 taskRoutes.get("/add", TaskController.createTask)
 taskRoutes.post("/add", TaskController.createTaskSave)
 taskRoutes.get("/", TaskController.showTasks)
+taskRoutes.post("/remove", TaskController.removeTask)
+taskRoutes.get("/edit/:id", TaskController.editTask)
+taskRoutes.post("/edit", TaskController.editTaskPost)
+taskRoutes.post("/done", TaskController.doneTask)
 
 export default taskRoutes
