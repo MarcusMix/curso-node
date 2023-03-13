dd2fe1ac9492559060654ed6b8bc12e18ac1bf64
Marcus Sandi <tradedomarcus@gmail.com>
2023-03-13 09:39:02 -0300

    feat: adding create task save

--- 10-mvc/routes/taskRoutes.js
+++ 10-mvc/routes/taskRoutes.js
@@ -4,6 +4,7 @@ const taskRoutes = e.Router()
 import TaskController from "../controllers/TaskController.js"
 
 taskRoutes.get("/add", TaskController.createTask)
+taskRoutes.post("/add", TaskController.createTaskSave)
 taskRoutes.get("/", TaskController.showTasks)
 
 export default taskRoutes
