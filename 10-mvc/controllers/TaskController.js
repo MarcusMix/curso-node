22f4f6e9c25369e627c44150f91cf4bed17254fa
Marcus Sandi <tradedomarcus@gmail.com>
2023-03-13 08:32:17 -0300

    feat: add new route tasks/all

--- 10-mvc/controllers/TaskController.js
+++ 10-mvc/controllers/TaskController.js
@@ -4,6 +4,10 @@ class TaskController {
     static createTask(req, res) {
         res.render("tasks/create")
     }
+
+    static showTasks(req, res) {
+        res.render("tasks/all")
+    }
 }
 
 export default TaskController
